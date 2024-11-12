447675dbe10d5266dc68e2dd30a118aa2a52fe88
ChandiniD-wq <chandinidatla53@gmail.com>
2024-11-12 16:10:01 -0700

    Add files via upload

---
+++ home.js
@@ -0,0 +1,12 @@
+function redirectToDog() {
+    window.location.href = "https://www.livescience.com/facts-about-dogs";
+  }
+  
+  function redirectToCat() {
+    window.location.href = "https://www.alleycat.org/resources/the-natural-history-of-the-cat/";
+  }
+  
+  function redirectToOtherAnimals() {
+    window.location.href = "https://en.wikipedia.org/wiki/Pet";
+  }
+  
